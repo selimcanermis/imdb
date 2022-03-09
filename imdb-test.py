eb8ddf0667b14ba8da7dbdb2b928f0b534f13927
selimcanermis <ermisselimcan@gmail.com>
2022-03-10 02:44:27 +0300

    Edit Codes

--- imdb-test.py
+++ imdb-test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ChromeOptions
 import time
 
+
 option = ChromeOptions()
 option.add_argument("--disable-infobars")
 option.add_argument("start-maximized")
@@ -12,15 +13,15 @@ option.add_argument("--disable-extensions")
 option.add_argument("--disable-notifications")
 
  
-PATH = Service(r'C:\Program Files\chromedriver.exe')
-driver = webdriver.Chrome(service=PATH)
+PATH = (r'C:\Program Files\chromedriver.exe')
+driver = webdriver.Chrome(chrome_options=option, executable_path=PATH)
 
 url_imdb = "https://www.imdb.com/"
 url_google = "https://www.google.com/"
 
 driver.get(url_imdb)
 print(driver.title)
-
+time.sleep(3)
 
 '''
 search = "dolar"
@@ -29,12 +30,12 @@ searchKeyInputClick = driver.find_element_by_xpath("/html/body/div[1]/div[3]/for
 '''
 
 menu = driver.find_element_by_xpath('//*[@id="imdbHeader-navDrawerOpen--desktop"]/div').click()
-time.sleep(5)
+time.sleep(3)
 top250 = driver.find_element_by_xpath('//*[@id="imdbHeader"]/div[2]/aside/div/div[2]/div/div[1]/span/div/div/ul/a[3]/span').click()
-time.sleep(5)
+time.sleep(3)
 
 rank = "rank alinamadi"
-#rank =driver.find_element_by_xpath('//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]/td[2]/text()').text
+#rank =driver.find_element_by_xpath('/*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]/td[2]').text
 #name = driver.find_elements_by_xpath('//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]/td[2]/a').text
 #rating = driver.find_elements_by_xpath('//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]/td[3]').text
 
@@ -42,6 +43,18 @@ print(rank)
 #print(name)
 #print(rating)
 
+#//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]/td[2]
+#//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[2]/td[2]
 
-time.sleep(15)
+#//*[@id="main"]/div/span/div/div/div[3]/table/tbody/tr[1]
+
+
+
+for i in range(1,10):
+    name = driver.find_elements_by_xpath("//*[@id='main']/div/span/div/div/div[3]/table/tbody/tr["+ str(i) +"]/td[2]")
+    print(name[0].text)
+    time.sleep(2)
+
+
+time.sleep(5)
 driver.quit()
